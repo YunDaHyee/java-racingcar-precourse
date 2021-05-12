d1c1bd0d328082b0f7dbb8177fb5bac58df0b27d
YunDaHyee <yon914@naver.com>
2021-05-12 17:17:01 +0900

    refactor: 프로덕트 코드 변경에 따른 테스트 코드 변경
    생성자에서 validName()로뺀 것에 대해 테스트 코드도 함께 변경

--- src/test/java/util/UtilTest.java
+++ src/test/java/util/UtilTest.java
@@ -16,32 +16,28 @@ import org.junit.jupiter.api.Test;
 public class UtilTest {
 	@Test
 	public void valid_name_length_test() {
-		/*		
-		 	assertThat(ValidationUtil.validName("car1")).isTrue(); // TDD 싸이클 2단계 - 성공
-			assertThat(ValidationUtil.validName(" ")).isFalse(); // TDD 싸이클 1단계 - 실패 - 공백에 대한 예외처리가 필요하다.
-			assertThat(ValidationUtil.validName("")).isFalse(); // TDD 싸이클 1단계
-			assertThat(ValidationUtil.validName("car22222222222")).isFalse(); // TDD 싸이클 2단계 - 성공
-		*/
-		// 공백에 대한 예외를 생성자에서 원시값 포장에 의한 방법으로 처리하기 위해 validName() 삭제
-		assertThat(new ValidationUtil("car1"));
-		assertThat(new ValidationUtil(" "));
-		assertThat(new ValidationUtil(""));
-		assertThat(new ValidationUtil("car22222222222"));
+		/* ▣ */
+		assertThat(ValidationUtil.validName("car1"));
+		assertThat(ValidationUtil.validName(" "));
+		assertThat(ValidationUtil.validName(""));
+		assertThat(ValidationUtil.validName("car22222222222"));
 	}
 	
 	@Test
 	public void split_name_test() {
-		//assertThat(new ValidationUtil("car1,car2,car3")); // TDD 싸이클 1 - 실패
-		assertThat(new ValidationUtil("car1,car2,car3")); // TDD 싸이클 2 - 성공 - NameRules.java 로직 수정
+		/* ▣ */
+		assertThat(ValidationUtil.validName("car1,car2,car3"));
 		
 		assertThatThrownBy( () -> {
-			new ValidationUtil(" ");
-			new ValidationUtil("");
+			ValidationUtil.validName(" ");
+			ValidationUtil.validName("");
 		}).isInstanceOf(IllegalArgumentException.class).hasMessage("자동차의 이름을 입력해야 합니다.");
 		
 		
 		assertThatThrownBy( () -> {
-			assertThat(new ValidationUtil("car1,car2,car32222222222222"));
+			assertThat(ValidationUtil.validName("car1,car2,car32222222222222"));
 		}).isInstanceOf(IllegalArgumentException.class).hasMessage("자동차의 이름은 1자 이상, 5자 이하여야 합니다.");
 	}
+	
+
 }

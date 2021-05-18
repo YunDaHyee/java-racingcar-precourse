c04354003ebd480a80c773d0c538d2ffb3a64024
YunDaHyee <yon914@naver.com>
2021-05-18 17:44:10 +0900

    test: 횟수에 관한 테스트 코드
     1~N 사이의 값이 아닐 때에 대한 테스트

--- src/main/java/util/ValidationUtil.java
+++ src/main/java/util/ValidationUtil.java
@@ -1,11 +1,13 @@
 package util;
 
 public class ValidationUtil {
-	private final static int MAX_LENGTH_COUNT = 9;
-	private final static int MIN_LENGTH_COUNT = 0;
 	
 	// TDD 싸이클 3단계 - 리팩토링 : ▣
 	public static String[] validName(String name) {
 		return new NameRules(name).names;
+	}
+
+	public static int validCount(int count) {
+		return new CountRules(count).count;
 	}
 }

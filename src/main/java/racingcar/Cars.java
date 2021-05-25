5a791e7c6f7523feec95c81bb0473770b97c7011
YunDaHyee <yon914@naver.com>
2021-05-25 16:57:04 +0900

    feat: winner를 가져오기 위한 전체적인 기능 수정 및 추가
    - maxPosition 변수 추가 : 모든 차들의 위치 중 가장 큰 위치값
    - isMaxPosition(), setMaxPosition() 추가: 현재 자동차 객체의 위치의 최대값 여부를 판단하고 최대값을 설정
    - play() 수정 : count 만큼 돌아도 랜덤값으로 자동차를 전진하는 것이기 때문에 승자가 1명이 나오지 않을 수 있음.
    이때 getWinner()에서 StringBuilder 객체가 빈 값일 때 play()를 호출하도록 했었는데 이는 결국 getWinner()도 그 호출 수만큼 호출되므로 비효율적인 것으로 판단하여 삭제.
    대신 최대값이 곧 count 수가 될 때까지 play()를 진행하도록 수정.

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -13,41 +13,54 @@ import java.util.List;
 
 public class Cars {
 	private int count;
+	private int maxPosition;
 	private final List<Car> cars;
 	
 	public Cars(int count, String names) {
 		this.count = count;
+		this.maxPosition = 0;
 		cars =  new LinkedList<Car>();
 		String[] splitNames = ValidationUtil.validName(names);
 		for( int i=0;i<splitNames.length;i++ ) {
 			this.cars.add(new Car(splitNames[i]));
 		}
-		
 	}
 	
-	public List<String> play() {
-		//for( int i=0;i<count;i++ ) {
-			for( int j=0;j<cars.size();j++ ) {
-				cars.get(j).go(randomValue());
-			}
-		//}
-		return getWinner();
-	}
-	
-	private List<String> getWinner() {
-		List<String> winners = new ArrayList<String>();
-		for( int i=0;i<cars.size();i++ ) {
-			if( cars.get(i).isWinner(count) ){
-				winners.add(cars.get(i).getName());
-			}
-		}
-		if( winners.isEmpty() ){
-			play();
-		}
-		return winners;
+	private void setMaxPosition() {
+		this.maxPosition++;
 	}
 	
 	private int randomValue() {
 		return new Random().nextInt(8) + 1;
 	}
+	
+	private boolean isMaxPosition(int position) {
+		return maxPosition<position;
+	}
+	
+	private String getWinner() {
+		//List<String> winners = new ArrayList<String>();
+		StringBuilder winners = new StringBuilder();
+		for( int i=0;i<cars.size();i++ ) {
+			if( cars.get(i).isWinner(count) ){
+				winners.append(cars.get(i).getName());
+			}
+		}
+
+		return winners.toString();
+	}
+	
+	public String play() {
+		while(maxPosition!=count){
+			for( int j=0;j<cars.size();j++ ) {
+				if( cars.get(j).go(randomValue()).isGo() ) {
+					if( isMaxPosition(cars.get(j).getPosition()) ) {
+						setMaxPosition();
+					}
+				}
+			}
+		}
+		return getWinner();
+	}
 }
+

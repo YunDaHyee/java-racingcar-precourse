7383fac28492549121bbc53b9f097f566deb78c4
YunDaHyee <yon914@naver.com>
2021-06-04 18:40:05 +0900

    test: Cars 생성자 인자 변경에 따른 파라미터 변경 및 bar 테스트코드 추가

--- src/test/java/core/CarsTest.java
+++ src/test/java/core/CarsTest.java
@@ -12,11 +12,24 @@ public class CarsTest {
 
 	@BeforeEach
 	void setUp() {
-		cars = new Cars(5, "car1,car2,car3");
+		String[] names = {"car1","car2","car3"};
+		cars = new Cars(/*5,*/names);
 	}
 
 	@Test
 	public void name_test() {
 		assertThat(cars.play()).contains("car1");
+	}
+	
+	@Test
+	public void bar_test() {
+		int count = 0;
+		StringBuilder sb = new StringBuilder();
+		while (count++ < 5) {
+			for (int i = 0; i < cars.cars.size(); i++) {
+				//sb.append(cars.getBar(cars.cars.get(i)));
+			}
+		}
+		assertThat(sb.toString()).contains("---");
 	}
 }

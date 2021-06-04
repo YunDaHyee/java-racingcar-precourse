348ee67825e30cfad6e880c0db435809665b4115
YunDaHyee <yon914@naver.com>
2021-06-04 17:42:49 +0900

    feat: 메인프로그램
    - 자동차 경주를 진행하기 위한 메인프로그램
    - 중복되는 출력 부분은 메서드로 분리

--- src/main/java/StartingLine.java
+++ src/main/java/StartingLine.java
@@ -5,18 +5,34 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import racingcar.Cars;
+import util.ValidationUtil;
 
 public class StartingLine {
-	public static void main(String args[]) throws IOException {
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter print = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedWriter print = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		print.write("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n");
-		String names = input.readLine();
-		print.write("시도할 회수는 몇 회인가요?");
-		int count = Integer.parseInt(input.readLine());
-		Cars cars = new Cars(count, names);
-		print.write("실행 결과");
-
+	private static void printContent(String content) throws IOException {
+		print.write(content);
+		print.flush();
 	}
+	
+	public static void main(String args[]) throws IOException {
+		printContent("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n");
+		String[] names = ValidationUtil.validName(input.readLine());
+		
+		printContent("시도할 회수는 몇 회인가요?\n");
+		int count = ValidationUtil.validCount(input.readLine());
+		
+		Cars cars = new Cars(names);
+		
+		for( int i=0;i<count;i++ ) {
+			printContent(cars.play());
+		}
+		
+		printContent( cars.getWinner() );
+		
+		print.close();
+		input.close();
+	}
+	
 }
